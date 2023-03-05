09c10482ecfcaa76a98941b5c60d19cf71313a4e
山口 聖也 <gssn724@outlook.jp>
2023-03-05 17:27:50 +0900

    MyHomePageからHomePageに名称変更

--- views/sound_minutes/lib/main.dart
+++ views/sound_minutes/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'presentations/widgets/MyHomepage.dart';
+import 'presentations/widgets/HomePage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
