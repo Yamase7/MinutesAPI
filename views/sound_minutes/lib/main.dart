6c1b66586fd5990b6ce899cdf8433fdb0ee577d6
山口 聖也 <gssn724@outlook.jp>
2023-03-05 17:40:45 +0900

    - タイトルを変更 - ファイル名を変更

--- views/sound_minutes/lib/main.dart
+++ views/sound_minutes/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'presentations/widgets/HomePage.dart';
+import 'presentations/widgets/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SoundMinute());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class SoundMinute extends StatelessWidget {
+  const SoundMinute({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SoundMinute',
       theme: ThemeData(
         // This is the theme of your application.
         //
--- views/sound_minutes/test/widget_test.dart
+++ views/sound_minutes/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:sound_minutes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SoundMinute());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
